1fcf54dd9e1c7ee4beadf3127cecf80e43fc3dce
Trevor Dorn <dorntrevor7@gmail.com>
2020-04-10 22:23:51 -0700

    style nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,10 @@ import { Navbar, Nav } from "react-bootstrap";
 
 export default function navbar() {
   const stylesr = {
+    fontSize: "40px",
     marginLeft: "60px",
     marginTop: "20px",
-    marginBottom: "20px",
+    textShadow: "3px 3px #a78334",
   };
   const stylesl = {
     marginRight: "80px",
@@ -15,7 +16,12 @@ export default function navbar() {
   return (
     <>
       <Router>
-        <Navbar bg="dark" variant="dark" className="colorfonts">
+        <Navbar
+          bg="dark"
+          variant="dark"
+          className="colorfonts"
+          style={{ boxShadow: "0px 10px 10px 10px #343a40" }}
+        >
           <Navbar.Brand href="#home" style={stylesr}>
             Trevor Dorn
           </Navbar.Brand>
