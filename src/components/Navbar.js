826c07391748c8d655e0599055a4d795c173a433
Trevor Dorn <dorntrevor7@gmail.com>
2020-04-10 22:03:51 -0700

    navbar contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function navbar() {
           <Nav className="ml-auto" style={stylesl}>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#Contact">Contact</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar>
       </Router>
